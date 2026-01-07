2b469b7a6afa20c4cdb2e569e0dafbea5b420d3b
ryan0122 <your@email.com>
2026-01-06 17:37:28 -0700

    fix build for netlify

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,8 +4,6 @@ const config: StorybookConfig = {
   stories: ['../packages/design-system/src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
   ],
